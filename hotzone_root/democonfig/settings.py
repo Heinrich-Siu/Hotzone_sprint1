39e5150a450309bce1377291ebcfb9c1bcf65573
Hezz <heinrich@connect.hku.hk>
2020-10-31 10:33:51 +0800

    change key

--- hotzone_root/democonfig/settings.py
+++ hotzone_root/democonfig/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kj&=x7erb+a))^8$@+2pq*b)d#32go7@((6wl%$+evz9g%p==d'
+SECRET_KEY = env('HOTZONE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["0.0.0.0", "hot-zone-hezz.herokuapp.com", '127.0.0.1', 'localhost']
 
